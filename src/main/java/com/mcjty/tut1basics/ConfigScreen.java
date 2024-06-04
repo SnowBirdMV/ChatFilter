8aac898889d0ebd9874ef9708d3fe9d0466ff8f1
Matthew Vogt <mvogt0000@gmail.com>
2024-06-04 18:51:25 -0400

    Added a multi-line textbox

--- src/main/java/com/mcjty/tut1basics/ConfigScreen.java
+++ src/main/java/com/mcjty/tut1basics/ConfigScreen.java
@@ -5,35 +5,32 @@ import java.util.Map;
 import java.util.function.Supplier;
 
 import com.mcjty.tut1basics.client.data.FilterRule;
+import com.mcjty.tut1basics.client.gui.MultiLineEditBox;
 import com.mcjty.tut1basics.client.gui.ScrollingList;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.Checkbox;
 import net.minecraft.client.gui.components.EditBox;
-import net.minecraft.client.gui.screens.*;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
 public class ConfigScreen extends Screen {
-    private static final Component CREDITS_AND_ATTRIBUTION = Component.translatable("options.credits_and_attribution");
-    private static final Component ENABLE_MOD = Component.translatable("options.chatfilter.enablemod");
-    private static final int COLUMNS = 2;
     private ScrollingList scrollingList;
     private final ChatFilterOptions options;
     private EditBox nameBox;
-    private EditBox filterValueBox;
+    private MultiLineEditBox filterValueBox;
     private Checkbox isRegexCheckbox;
     private Checkbox isOverlayCheckbox;
-    public boolean test = true;
+
     public ConfigScreen(ChatFilterOptions chatFilterOptions) {
         super(Component.translatable("options.title"));
         this.options = chatFilterOptions;
-
-
     }
 
+    @Override
     protected void init() {
         super.init();
         // Add a button
@@ -51,30 +48,27 @@ public class ConfigScreen extends Screen {
         this.scrollingList = new ScrollingList(this, 100, this.height, 32, this.height - 32, 20, filterMap, this::onFilterListItemSelected);
         this.addRenderableWidget(this.scrollingList);
 
-        // Add a text input box
+        // Add multi-line text input boxes
         this.nameBox = new EditBox(this.font, 120, 40, 200, 20, Component.translatable("options.chatfilter.namebox"));
-        this.nameBox.setMaxLength(Short.MAX_VALUE);
         this.addRenderableWidget(this.nameBox);
-        this.filterValueBox = new EditBox(this.font, 120, 80, 200, 20, Component.translatable("options.chatfilter.filtervaluebox"));
-        this.filterValueBox.setMaxLength(Short.MAX_VALUE);
+        this.filterValueBox = new MultiLineEditBox(this.font, 120, 80, 200, 100, Component.translatable("options.chatfilter.filtervaluebox"));
         this.addRenderableWidget(this.filterValueBox);
+
+        // Add checkboxes
         this.isRegexCheckbox = new Checkbox(120, this.height - 50, 20, 20, Component.translatable("options.chatfilter.isregexcheckbox"), false);
         this.addRenderableWidget(this.isRegexCheckbox);
         this.isOverlayCheckbox = new Checkbox(220, this.height - 50, 20, 20, Component.translatable("options.chatfilter.isoverlaycheckbox"), false);
         this.addRenderableWidget(this.isOverlayCheckbox);
-
     }
 
-
-
-
+    @Override
     public void removed() {
-//        this.options.save();
+        // this.options.save();
     }
 
     private void onFilterListItemSelected(ScrollingList.Entry entry) {
         // Handle the selection event here
-        FilterRule rule = (FilterRule)entry.getValue();
+        FilterRule rule = (FilterRule) entry.getValue();
         System.out.println("Item selected: " + entry.getText().getString());
         this.nameBox.setValue(rule.getName());
         this.filterValueBox.setValue(rule.getFilterText());
@@ -86,13 +80,27 @@ public class ConfigScreen extends Screen {
         }
     }
 
+    @Override
+    public boolean mouseScrolled(double pMouseX, double pMouseY, double pScrollX, double pScrollY) {
+        if (this.filterValueBox.isMouseOver(pMouseX, pMouseY)) {
+            this.filterValueBox.scroll((int) -pScrollY);
+            return true;
+        }
+        return super.mouseScrolled(pMouseX, pMouseY, pScrollX, pScrollY);
+    }
+
+    @Override
     public void render(GuiGraphics pGuiGraphics, int mouseX, int mouseY, float partialTicks) {
         super.render(pGuiGraphics, mouseX, mouseY, partialTicks);
         pGuiGraphics.drawCenteredString(this.font, this.title, this.width / 2, 15, 16777215);
         this.scrollingList.render(pGuiGraphics, mouseX, mouseY, partialTicks);
-        super.render(pGuiGraphics, mouseX, mouseY, partialTicks);
+        this.nameBox.render(pGuiGraphics, mouseX, mouseY, partialTicks);
+        this.filterValueBox.render(pGuiGraphics, mouseX, mouseY, partialTicks);
+        this.isRegexCheckbox.render(pGuiGraphics, mouseX, mouseY, partialTicks);
+        this.isOverlayCheckbox.render(pGuiGraphics, mouseX, mouseY, partialTicks);
     }
 
+    @Override
     public void onClose() {
         this.minecraft.setScreen(null);
     }
